2ed4589cf8ac9ec2e4d1024c79856eceb44e0c28
naveen5005 <100987924+naveen5005@users.noreply.github.com>
2024-03-20 01:33:04 +0530

    Registration

--- ReactJS_Project/crm-salesforce-application/src/App.js
+++ ReactJS_Project/crm-salesforce-application/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Registration from './Components/Registration';
+import Login from './Components/Login';
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Registration />} />
+        <Route path='/login' element={<Login/>}/>
       </Routes>
     </Router>
   );
