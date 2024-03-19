43dfaeaba14abde2ee4aaeabe5b4fb57c93a21a4
naveen5005 <100987924+naveen5005@users.noreply.github.com>
2024-03-19 16:01:51 +0530

    Registration page

---
+++ ReactJS_Project/crm-salesforce-application/src/Components/Registration.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+import '../Styles/Registration.css'
+import { Button, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, TextField } from '@mui/material';
+
+const Registration = () => {
+    const [registrationUser, setRegistrationUser] = useState({
+        fname: "",
+        lname: "",
+        email: "",
+        pwd: "",
+        confirmPwd: "",
+        gender: ""
+    })
+    const [passwordMatchError, setPasswordMatchError] = useState('');
+
+    const handleChange = (e) => {
+        const newUser = { ...registrationUser };
+        newUser[e.target.name] = e.target.value;
+        setRegistrationUser(newUser);
+        // Check if password and confirm password match
+        if (e.target.name === 'confirmPwd' && e.target.value !== registrationUser.pwd) {
+            setPasswordMatchError('Passwords do not match');
+        } else {
+            setPasswordMatchError('');
+        }
+    }
+    const handleRegisterUser = () => {
+        console.log(registrationUser)
+    }
+    return (
+        <div style={{ marginTop: '50px' }}>
+            <h2 style={{ textAlign: 'center' }}>Welcome to the Registration page</h2>
+            <div className='registration'>
+                <FormControl>
+                    <TextField className='textField' label="FirstName" type='text' variant="outlined" value={registrationUser.fname} name='fname' onChange={handleChange} />
+                    <TextField className='textField' label="LastName" type='text' variant="outlined" value={registrationUser.lname} name='lname' onChange={handleChange} />
+                    <TextField className='textField' label="Email" type='email' variant='outlined' value={registrationUser.email} name='email' onChange={handleChange} />
+                    <TextField className='textField' label="Password" type='password' variant='outlined' value={registrationUser.pwd} name='pwd' onChange={handleChange} />
+                    <TextField className='textField' label="Confirm Password" type='password' variant='outlined' value={registrationUser.confirmPwd} name='confirmPwd' onChange={handleChange} error={passwordMatchError !== ''} helperText={passwordMatchError} />
+
+                    <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
+                    <RadioGroup
+                        aria-labelledby="demo-radio-buttons-group-label"
+                        name="radio-buttons-group"
+                    >
+                        <FormControlLabel value="female" control={<Radio />} label="Female" name='gender' onChange={handleChange} checked={registrationUser.gender === "female"} />
+                        <FormControlLabel value="male" control={<Radio />} label="Male" name='gender' onChange={handleChange} checked={registrationUser.gender === "male"} />
+                        <FormControlLabel value="other" control={<Radio />} label="Other" name='gender' onChange={handleChange} checked={registrationUser.gender === "other"} />
+                    </RadioGroup>
+
+                    <Button variant='contained' onClick={handleRegisterUser}>Add User</Button>
+                </FormControl>
+            </div>
+        </div>
+    )
+}
+
+export default Registration
