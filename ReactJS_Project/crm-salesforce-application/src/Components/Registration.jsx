4a0c72f9d39a89672cd461ab93a7677803830aee
naveen5005 <100987924+naveen5005@users.noreply.github.com>
2024-03-20 01:31:37 +0530

    Registration - crm

--- ReactJS_Project/crm-salesforce-application/src/Components/Registration.jsx
+++ ReactJS_Project/crm-salesforce-application/src/Components/Registration.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import '../Styles/Registration.css'
-import { Button, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, TextField } from '@mui/material';
+import { Button, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, TextField, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const Registration = () => {
     const [registrationUser, setRegistrationUser] = useState({
@@ -12,6 +13,7 @@ const Registration = () => {
         gender: ""
     })
     const [passwordMatchError, setPasswordMatchError] = useState('');
+    const [registrationSuccess, setRegistrationSuccess] = useState(false)
 
     const handleChange = (e) => {
         const newUser = { ...registrationUser };
@@ -20,12 +22,17 @@ const Registration = () => {
         setRegistrationUser(newUser);
     }
     const handleRegisterUser = () => {
-        console.log(registrationUser)
+        console.log(registrationUser);
+        setRegistrationSuccess(true);
+
     }
     return (
         <div style={{ marginTop: '50px' }}>
             <h2 style={{ textAlign: 'center' }}>Welcome to the Registration page</h2>
             <div className='registration'>
+                {
+                    registrationSuccess && <Typography variant='h5' style={{ color: 'green', marginBottom: '20px' }}>Registration Successful!!!</Typography>
+                }
                 <FormControl>
                     <TextField className='textField' label="FirstName" type='text' variant="outlined" value={registrationUser.fname} name='fname' onChange={handleChange} />
                     <TextField className='textField' label="LastName" type='text' variant="outlined" value={registrationUser.lname} name='lname' onChange={handleChange} />
@@ -44,6 +51,12 @@ const Registration = () => {
                     </RadioGroup>
 
                     <Button variant='contained' onClick={handleRegisterUser}>Add User</Button>
+
+                    <div className='SignIn'>
+                        Already have an Account? <Link style={{textDecoration:'none',color:'darkblue'}} to={'/login'}>SignIn</Link>
+                    </div>
+
+
                 </FormControl>
             </div>
         </div>
